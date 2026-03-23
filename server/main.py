226ef742c5cbb21d21f6494e5154341b75234962
Max Westmoreland <mw.maxwestmoreland@gmail.com>
2026-03-23 13:32:44 -0500

    add deployed frontend origin and ignore vite cache

--- server/main.py
+++ server/main.py
@@ -49,6 +49,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://mountory-inventory.vercel.app",
+        "https://mountory-inventory-muttu5jls-maxw125s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
